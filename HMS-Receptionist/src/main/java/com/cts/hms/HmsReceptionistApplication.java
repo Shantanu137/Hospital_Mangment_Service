30897b1d522be03909c642055ecdba0ea602c080
Shantanu <shantanupadamwar137@gmail.com>
2024-03-30 11:50:44 +0530

    this microservice is for Receptionist

---
+++ HMS-Receptionist/src/main/java/com/cts/hms/HmsReceptionistApplication.java
@@ -0,0 +1,13 @@
+package com.cts.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsReceptionistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsReceptionistApplication.class, args);
+	}
+
+}
